ee46e33a031d60282ddc5ba79ab29024554e0b26
benjaminsinaiko <ben.jamin@outlook.com>
2018-07-19 12:28:26 -0500

    Logout and Conditional Navbar links

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,9 +16,11 @@ const User = require('../../models/User');
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-router.get('/test', (req, res) => res.json({
-  msg: 'Users Works'
-}));
+router.get('/test', (req, res) =>
+  res.json({
+    msg: 'Users Works'
+  })
+);
 
 // @route   POST api/users/register
 // @desc    Register user
@@ -33,37 +35,40 @@ router.post('/register', (req, res) => {
 
   User.findOne({
     email: req.body.email
-  })
-    .then(user => {
-      if (user) {
-        errors.email = 'Email already exists';
-        return res.status(400).json(errors);
-      } else {
-        const avatar = gravatar.url(req.body.email, {
+  }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    } else {
+      const avatar = gravatar.url(
+        req.body.email,
+        {
           s: '200', // Size
           r: 'pg', // Rating
           d: 'mm' // Default
-        });
+        },
+        false
+      );
 
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          avatar,
-          password: req.body.password
-        });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
         });
-      }
-    });
+      });
+    }
+  });
 });
 
 // @route   POST api/users/login
@@ -89,48 +94,52 @@ router.post('/login', (req, res) => {
     }
 
     // Check Password
-    bcrypt.compare(password, user.password)
-      .then(isMatch => {
-        if (isMatch) {
-          // User Matched
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User Matched
 
-          const payload = {
-            id: user.id,
-            name: user.name,
-            avatar: user.avatar
-          }; // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        }; // Create JWT Payload
 
-          // Sign Token
-          jwt.sign(
-            payload,
-            keys.secretOrKey, {
-              expiresIn: 3600
-            },
-            (err, token) => {
-              res.json({
-                success: true,
-                token: 'Bearer ' + token
-              });
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
             });
-        } else {
-          errors.password = 'Password incorrect';
-          return res.status(400).json({ errors });
-        }
-      });
+          }
+        );
+      } else {
+        errors.password = 'Password incorrect';
+        return res.status(400).json({ errors });
+      }
+    });
   });
 });
 
 // @route   GET api/users/current
 // @desc    Return current user
 // @access  Private
-router.get('/current',
-  passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
     // res.json(req.user);
     res.json({
       id: req.user.id,
       name: req.user.name,
       email: req.user.email
     });
-  });
+  }
+);
 
 module.exports = router;
