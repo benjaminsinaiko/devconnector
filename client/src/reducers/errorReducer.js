bff59f103bef78577c00b6a6ded68f6c73b22e65
benjaminsinaiko <ben.jamin@outlook.com>
2018-08-06 16:54:58 -0500

    Comment display and delete,  and clear errors

--- client/src/reducers/errorReducer.js
+++ client/src/reducers/errorReducer.js
@@ -1,4 +1,4 @@
-import { GET_ERRORS } from '../actions/types';
+import { GET_ERRORS, CLEAR_ERRORS } from '../actions/types';
 
 const initialState = {
   isAuthenticated: false,
@@ -7,9 +7,11 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case GET_ERRORS:
-      return action.payload;
-    default:
-      return state;
+  case GET_ERRORS:
+    return action.payload;
+  case CLEAR_ERRORS:
+    return {};
+  default:
+    return state;
   }
 }
